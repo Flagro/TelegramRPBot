fda1a4acc942fb2ed9ba4f405ad1a8f002548c2d
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-03-24 13:42:42 +0100

    Add support for multiple actions with the same modes keyboard

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -8,7 +8,10 @@ from .db.db import ChatModeResponse
 
 
 def get_chat_modes_keyboard(
-    chat_modes: List[ChatModeResponse], page_index: int, modes_per_page: int
+    chat_modes: List[ChatModeResponse],
+    page_index: int,
+    modes_per_page: int,
+    action: str,
 ) -> InlineKeyboardMarkup:
     page_start = page_index * modes_per_page
     page_end = (page_index + 1) * modes_per_page
@@ -18,7 +21,7 @@ def get_chat_modes_keyboard(
     for chat_mode in chat_modes[page_start:page_end]:
         modes.append(
             InlineKeyboardButton(
-                chat_mode.mode_name, callback_data=f"set_chat_mode|{chat_mode.id}"
+                chat_mode.mode_name, callback_data=f"{action}|{chat_mode.id}"
             )
         )
 
