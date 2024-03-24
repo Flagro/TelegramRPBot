b5c688b6ce635465377db7705b4de248921e9ef4
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-03-24 11:28:53 +0100

    Implement mode addition functionality

--- bot/bot.py
+++ bot/bot.py
@@ -133,8 +133,16 @@ class TelegramRPBot:
     @command_handler
     @authorized
     async def _addmode(self, chat_id, args) -> CommandResponse:
-        # Implement parse of a new mode here
-        pass
+        mode_description = " ".join(args)
+        # Get the first sentence or paragraph of the mode description:
+        # TODO: implement NER here
+        mode_name = mode_description.split("\n")[0].split(".")[0]
+        try:
+            self.db.add_mode(chat_id, mode_name, mode_description)
+            return CommandResponse("mode_added", {"mode_name": mode_name})
+        except ValueError as e:
+            self.logger.error(f"Error adding mode: {e}")
+            return CommandResponse("inappropriate_mode", {})
 
     @command_handler
     @authorized
