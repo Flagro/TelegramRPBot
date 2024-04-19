2317a790b1068eee5d38a0ade3606eb66933e96c
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-04-19 13:12:03 +0200

    Refactor MessageHandler to include wrapper method in base_handlers.py

--- bot/base_handlers.py
+++ bot/base_handlers.py
@@ -54,6 +54,34 @@ class MessageHandler(ABC):
         self.ai = ai
         self.localizer = localizer
 
+    async def wrapper(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        user_handle = "@" + update.message.from_user.username
+        chat_id = update.message.chat_id
+        thread_id = None
+        if update.effective_message and update.effective_message.is_topic_message:
+            thread_id = update.effective_message.message_thread_id
+        message = update.message.text
+
+        # get image and audio in memory
+        image = None
+        if update.message.photo:
+            image = get_file_in_memory(update.message.photo[-1].file_id, context)
+
+        voice = None
+        if update.message.voice:
+            voice = get_file_in_memory(update.message.voice.file_id, context)
+
+        return await self.handle(
+            self,
+            chat_id,
+            thread_id,
+            bot_mentioned(update, context),
+            user_handle,
+            message,
+            image,
+            voice,
+        )
+
     def handle(self, update, context):
         message = update.message
         chat_id = message.chat_id
--- bot/handler_decorators.py
+++ bot/handler_decorators.py
@@ -23,36 +23,3 @@ def command_handler(func):
         )
 
     return wrapper
-
-
-def message_handler(func):
-    @wraps(func)
-    async def wrapper(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        user_handle = "@" + update.message.from_user.username
-        chat_id = update.message.chat_id
-        thread_id = None
-        if update.effective_message and update.effective_message.is_topic_message:
-            thread_id = update.effective_message.message_thread_id
-        message = update.message.text
-
-        # get image and audio in memory
-        image = None
-        if update.message.photo:
-            image = get_file_in_memory(update.message.photo[-1].file_id, context)
-
-        voice = None
-        if update.message.voice:
-            voice = get_file_in_memory(update.message.voice.file_id, context)
-
-        return await func(
-            self,
-            chat_id,
-            thread_id,
-            bot_mentioned(update, context),
-            user_handle,
-            message,
-            image,
-            voice,
-        )
-
-    return wrapper
