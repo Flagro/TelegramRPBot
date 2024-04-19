1cbb60dcf192191786132b755d87b0fe44e82c43
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-04-19 13:12:44 +0200

    Refactor CommandHandler to include wrapper method in base_handlers.py

--- bot/base_handlers.py
+++ bot/base_handlers.py
@@ -108,6 +108,26 @@ class CommandHandler(ABC):
         self.ai = ai
         self.localizer = localizer
 
+    async def wrapper(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        sig = signature(self.handle)
+        params = {}
+        if "user_handle" in sig.parameters:
+            params["user_handle"] = update.effective_user.username
+        if "chat_id" in sig.parameters:
+            params["chat_id"] = update.effective_chat.id
+        if "args" in sig.parameters:
+            params["command_args"] = context.args
+        result = await self.handle(self, **params)
+        text_response, parse_mode = self.localizer.get_command_response(
+            result.text, result.kwargs
+        )
+
+        self.send_message(
+            chat_id=update.effective_chat.id,
+            text=text_response,
+            parse_mode=parse_mode,
+        )
+
     def handle(self, update, context):
         message = update.message
         chat_id = message.chat_id
