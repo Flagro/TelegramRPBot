d650b4d0df8875de71c2881d1e41bcbcc1563d6a
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-04-28 19:33:50 +0200

    Add open closed style inits

---
+++ bot/rp_bot/commands/__init__.py
@@ -0,0 +1,16 @@
+import os
+import importlib
+
+handlers = []
+
+directory = os.path.dirname(__file__)
+
+for filename in os.listdir(directory):
+    if filename.endswith("handler.py"):
+        module_name = filename[:-3]
+        module_path = f".{module_name}"
+        module = importlib.import_module(module_path, package='bot.rp_bot.commands')
+        
+        handler_class = getattr(module, 'CommandHandler', None)
+        if handler_class:
+            handlers.append(handler_class)
