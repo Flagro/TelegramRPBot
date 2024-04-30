9cae136354d207e3e3e613744f62c7cfb368a768
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-04-30 11:15:20 +0200

    Add config models and translations

---
+++ bot/models/config/__init__.py
@@ -0,0 +1,6 @@
+from .ai_config import AIConfig
+from .auth_config import AuthConfig
+from .db_config import DBConfig
+from .tg_config import TGConfig
+from .localizer_translations import LocalizerTranslations
+from .default_chat_modes import DefaultChatModes
