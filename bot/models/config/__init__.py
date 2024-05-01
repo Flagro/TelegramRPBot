ffa67bf22a8fedfe6ac9b2459108a2e1ceb63e95
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-05-01 18:07:43 +0200

    Remove main config

--- bot/models/config/__init__.py
+++ bot/models/config/__init__.py
@@ -1,5 +1,4 @@
 from .ai_config import AIConfig
-from .db_config import DBConfig
 from .tg_config import TGConfig
 from .localizer_translations import LocalizerTranslations
 from .default_chat_modes import DefaultChatModes
--- bot/models/config/db_config.py
+++
@@ -1,7 +0,0 @@
-from .base_config import BaseYAMLConfigModel
-
-
-class DBConfig(BaseYAMLConfigModel):
-    DB_SCHEME: str
-    DB_HOST: str
-    DB_PORT: int
