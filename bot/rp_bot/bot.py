37e71902926fed6e253996ef5f307c7bdcffa14b
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-05-06 18:12:55 +0200

    Refactor RPBot class to inherit from BaseBot and implement callback handlers

--- bot/rp_bot/bot.py
+++ bot/rp_bot/bot.py
@@ -1,3 +1,17 @@
-class RPBot:
+from typing import List
+from bot.models.base_handlers import BaseCallbackHandler
+from ..models.base_bot import BaseBot
+
+
+class RPBot(BaseBot):
     def __init__(self, ai, db, localizer, auth, logger):
         pass
+
+    def get_callbacks(self) -> List[BaseCallbackHandler]:
+        return super().get_callbacks()
+    
+    def get_commands(self) -> List[BaseCallbackHandler]:
+        return super().get_commands()
+
+    def get_messages(self) -> List[BaseCallbackHandler]:
+        return super().get_messages()
