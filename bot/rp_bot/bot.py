4bde071396337caa3e4b44ab2e9e609b1bfe7602
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-05-09 11:14:32 +0200

    Refactor RPBot class to initialize handlers and assign dependencies

--- bot/rp_bot/bot.py
+++ bot/rp_bot/bot.py
@@ -7,24 +7,16 @@ from .messages import handlers as message_handlers
 
 
 class RPBot(BaseBot):
+    callbacks = command_handlers
+    commands = callback_handlers
+    messages = message_handlers
+    
     def __init__(self, ai, db, localizer, auth, logger):
         # TODO: init here the handlers with parameters
         # and store them in a class attribute
-        self.callbacks = self.get_callbacks()
-        self.commands = self.get_commands()
-        self.messages = self.get_messages()
         for handler in self.callbacks + self.commands + self.messages:
             handler.ai = ai
             handler.db = db
             handler.localizer = localizer
             handler.auth = auth # TODO: init permissions
             handler.logger = logger
-
-    def get_callbacks(self) -> List[BaseCallbackHandler]:
-        return callback_handlers
-    
-    def get_commands(self) -> List[BaseCallbackHandler]:
-        return command_handlers
-
-    def get_messages(self) -> List[BaseCallbackHandler]:
-        return message_handlers
