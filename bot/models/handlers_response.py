3a638ccd9249875623e64ca7872cc484810fb951
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-05-14 08:16:51 +0200

    Refactor ListResponse constructor to include callback and button_action parameters

--- bot/models/handlers_response.py
+++ bot/models/handlers_response.py
@@ -1,11 +1,12 @@
 from typing import Optional, Any
 from uuid import UUID
 
+
 class CommandResponse:
     def __init__(self, text, kwargs: Optional[dict] = None):
         self.text = text
         self.kwargs = kwargs
-        
+
 
 class MessageResponse:
     def __init__(self, text, image_url: Optional[str] = None):
@@ -14,8 +15,18 @@ class MessageResponse:
 
 
 class ListResponse:
-    def __init__(self, text, kwargs: Optional[dict], names: list[str], ids: list[UUID]):
+    def __init__(
+        self,
+        text,
+        kwargs: Optional[dict],
+        names: list[str],
+        ids: list[UUID],
+        callback: str,
+        button_action: str,
+    ):
         self.text = text
         self.kwargs = kwargs
         self.names = names
         self.ids = ids
+        self.callback = callback
+        self.button_action = button_action
