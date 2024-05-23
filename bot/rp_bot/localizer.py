7bf0a89a13ffadb6c7f2aa9c56fb643f106e0d24
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-05-23 18:42:48 +0200

    Refactor Localizer class to accept LocalizerTranslations object instead of a dictionary

--- bot/rp_bot/localizer.py
+++ bot/rp_bot/localizer.py
@@ -1,9 +1,11 @@
 from .db import DB
+from ..models.config.localizer_translations import LocalizerTranslations
 
 
 class Localizer:
-    def __init__(self, db: DB, translations: dict) -> None:
+    def __init__(self, db: DB, translations: LocalizerTranslations) -> None:
         self.db = db
+        self.translations = translations
 
     def set_language(self, chat_id: str, language: str) -> None:
         self.db.set_language(chat_id, language)
