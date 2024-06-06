ac7192862357d462d24213536e9e8c675590ac85
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-06-06 21:20:33 +0200

    Merge keyboard and command response

--- bot/models/handlers_response.py
+++ bot/models/handlers_response.py
@@ -2,23 +2,20 @@ from typing import Optional, Dict
 from uuid import UUID
 
 
-class CommandResponse:
-    def __init__(self, text, kwargs: Optional[dict] = None):
-        self.text = text
-        self.kwargs = kwargs
-
-
 class KeyboardResponse:
     def __init__(
         self,
-        text,
-        kwargs: Optional[dict],
         modes_dict: Dict[UUID, str],
         callback: str,
         button_action: str,
     ):
-        self.text = text
-        self.kwargs = kwargs
         self.modes_dict = modes_dict
         self.callback = callback
         self.button_action = button_action
+
+
+class CommandResponse:
+    def __init__(self, text, kwargs: Optional[dict] = None, keyboard: Optional[KeyboardResponse] = None):
+        self.text = text
+        self.kwargs = kwargs
+        self.keyboard = keyboard
