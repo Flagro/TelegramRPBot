7327fa330cbdeaf11404374ee1e13bc8faa37872
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-06-11 21:28:02 +0200

    Fix the default chat modes structure

--- bot/models/config/default_chat_modes.py
+++ bot/models/config/default_chat_modes.py
@@ -1,9 +1,16 @@
 from .base_config import BaseYAMLConfigModel
+from typing import Dict
+from pydantic import BaseModel
+
+
+class ChatMode(BaseModel):
+    name: str
+    model_type: str = None
+    welcome_message: str
+    prompt_start: str
+    parse_mode: str = None
 
 
 class DefaultChatModes(BaseYAMLConfigModel):
-    name: str
-    model_type: str
-    welcome_message: str
-    prompt_start: str
-    parse_mode: str
+    assistant: ChatMode
+    motivator: ChatMode
