8691112c19772a90b06987526c0e048fe7ec7794
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-06-14 20:55:45 +0200

    Implement the missing abstract methods

--- bot/rp_bot/bot.py
+++ bot/rp_bot/bot.py
@@ -10,7 +10,16 @@ class RPBot(BaseBot):
     callbacks = command_handlers
     commands = callback_handlers
     messages = message_handlers
-    
+
+    def get_callbacks(self) -> List[BaseCallbackHandler]:
+        return self.callbacks
+
+    def get_commands(self) -> List[BaseCallbackHandler]:
+        return self.commands
+
+    def get_messages(self) -> List[BaseCallbackHandler]:
+        return self.messages
+
     def __init__(self, ai, db, localizer, auth, logger):
         # TODO: init here the handlers with parameters
         # and store them in a class attribute
@@ -18,5 +27,5 @@ class RPBot(BaseBot):
             handler.ai = ai
             handler.db = db
             handler.localizer = localizer
-            handler.auth = auth # TODO: init permissions
+            handler.auth = auth  # TODO: init permissions
             handler.logger = logger
