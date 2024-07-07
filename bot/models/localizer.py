29f88bf336ac178fdf09c9cf3cb801c70f05ad35
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-07-07 17:16:01 +0200

    Make localizer async

--- bot/models/localizer.py
+++ bot/models/localizer.py
@@ -8,8 +8,8 @@ class Localizer:
         self.db: DB = db
         self.translations: LocalizerTranslations = translations
 
-    def set_language(self, context: Context, language: str) -> None:
-        self.db.set_language(context, language)
+    async def set_language(self, context: Context, language: str) -> None:
+        await self.db.set_language(context, language)
 
     def compose_user_input(
         self, message: str, image_description: str, voice_description: str
@@ -18,8 +18,8 @@ class Localizer:
             f"{message}\n{image_description}\n{voice_description}"
         )
         
-    def compose_history_message(self, context: Context) -> str:
-        messages = self.db.get_messages(context)
+    async def compose_history_message(self, context: Context) -> str:
+        messages = await self.db.get_messages(context)
         return "\n".join(messages)
 
     def get_command_response(self, text: str, kwargs: dict) -> str:
