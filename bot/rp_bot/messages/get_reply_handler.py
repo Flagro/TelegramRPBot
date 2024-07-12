8ef94f28c44a3ac6fb4345d816c6fb30282abec3
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-07-12 18:33:32 +0200

    Refactor the get_reply function

--- bot/rp_bot/messages/get_reply_handler.py
+++ bot/rp_bot/messages/get_reply_handler.py
@@ -17,19 +17,23 @@ class MessageHandler(BaseMessageHandler):
         message: Message,
         args: List[str],
     ) -> Optional[CommandResponse]:
-        if context.is_bot_mentioned:
-            image_description = await self.ai.describe_image(message.in_file_image) if message.in_file_image else None
-            voice_description = await self.ai.transcribe_audio(message.in_file_audio) if message.in_file_audio else None
-            user_input = self.localizer.compose_user_input(
-                message, image_description, voice_description
-            )
-            response_message, response_image_url = await self.ai.get_reply(
-                user_input
-            )
-            await self.db.add_user_message_to_dialog(context, person, user_input)
-            await self.db.add_bot_response_to_dialog(
-                context, response_message, response_image_url
-            )
-        if await self.db.get_conversation_tracker_state(context):
-            await self.db.add_user_message_to_dialog(context, person, message.message_text)
+        conversation_tracker_enabled = await self.db.get_conversation_tracker_state(context)
+        if not conversation_tracker_enabled and not context.is_bot_mentioned:
+            return None
+        # Note that here the responsibility to pass NULL images and Audio is on the
+        # outer level bot processing (TG bot or other bot)
+        image_description = await self.ai.describe_image(message.in_file_image) if message.in_file_image else None
+        voice_description = await self.ai.transcribe_audio(message.in_file_audio) if message.in_file_audio else None
+        user_input = self.localizer.compose_user_input(
+            message, image_description, voice_description
+        )
+        await self.db.add_user_message_to_dialog(context, person, user_input)
+        if not context.is_bot_mentioned:
+            return None
+        response_message, response_image_url = await self.ai.get_reply(
+            user_input
+        )
+        await self.db.add_bot_response_to_dialog(
+            context, response_message, response_image_url
+        )
         return CommandResponse("message_response", {"response_text": response_message})
