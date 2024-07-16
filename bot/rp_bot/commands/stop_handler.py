2b27b764d56ae6abe20104a0525dc95b0180119a
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-07-16 18:23:47 +0200

    Add start and stop handlers

---
+++ bot/rp_bot/commands/stop_handler.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from ...models.base_handlers import BaseCommandHandler
+from ...models.handlers_response import CommandResponse
+from ...models.handlers_input import Person, Context, Message
+from ..auth import BotAdmin
+
+
+class CommandHandler(BaseCommandHandler):
+    permissions = [BotAdmin]
+    command = "stop"
+    list_priority_order = 1
+
+    async def get_command_response(
+        self, person: Person, context: Context, message: Message, args: List[str]
+    ) -> CommandResponse:
+        await self.db.stop_chat(context)
+        return CommandResponse("stop_done", {})
