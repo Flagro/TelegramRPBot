73d3f123abee38022ba2422fdfe9d71bd98b0d66
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-07-22 00:18:11 +0200

    Refactor the methods to use new models

--- bot/rp_bot/db_models/chats.py
+++ bot/rp_bot/db_models/chats.py
@@ -1,7 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from typing import Optional, List
 from bson import ObjectId
-from uuid import UUID
 from collections import namedtuple
 from .base_models import BaseModel
 from ...models.config import DefaultChatModes
@@ -18,54 +17,81 @@ class Chats(BaseModel):
         self, db: AsyncIOMotorDatabase, default_chat_modes: DefaultChatModes
     ) -> None:
         super().__init__(db)
+        self.chats = db.chats
         self.chat_modes = db.chat_modes
+        self.user_facts = db.user_facts
+        self.user_introductions = db.user_introductions
         self.default_chat_modes = default_chat_modes
 
     def _init_default_chat_modes(self, context: Context) -> None:
-        chat_id = context.chat_id
         for _, mode in self.default_chat_modes.default_chat_modes.items():
             self.chat_modes.update_one(
-                {"chat_id": chat_id, "mode_name": mode.name},
+                {"chat_id": context.chat_id, "mode_name": mode.name},
                 {"$setOnInsert": {"mode_description": mode.description}},
                 upsert=True,
             )
 
     async def create_chat_if_not_exists(self, context: Context) -> None:
+        """Initialize default parameters for a chat if it does not exist
+        Including:
+        - initialization of default chat modes
+        - initialization of
+        - initialization of conversation tracker
+        - initialization of language
+        - initialization of started chat
+
+        Args:
+            context (Context): context of the chat
+        """
         self._init_default_chat_modes(context)
+        chat_id = context.chat_id
+        await self.chats.update_one(
+            {"chat_id": chat_id},
+            {"$setOnInsert": {"chat_id": chat_id}},
+            upsert=True,
+        )
 
     async def start_chat(self, context: Context) -> None:
         chat_id = context.chat_id
-        await self.chat_modes.update_one(
-            {"chat_id": chat_id}, {"$set": {"chat_started": True}}, upsert=True
+        await self.chats.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"is_started": True}},
         )
 
     async def chat_is_started(self, context: Context) -> bool:
+        """Check if chat is started
+        by checking the flag is_started in the chat document
+        """
         chat_id = context.chat_id
-        chat_data = await self.chat_modes.find_one({"chat_id": chat_id})
-        return chat_data.get("chat_started", False)
+        chat_data = await self.chats.find_one(
+            {"chat_id": chat_id}, {"_id": 0, "is_started": 1}
+        )
+        return chat_data.get("is_started", False)
+        
 
     async def stop_chat(self, context: Context) -> None:
         chat_id = context.chat_id
-        await self.chat_modes.update_one(
-            {"chat_id": chat_id}, {"$set": {"chat_started": False}}
+        await self.chats.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"is_started": False}},
         )
 
     async def set_language(self, context: Context, language: str) -> None:
         chat_id = context.chat_id
-        await self.chat_modes.update_one(
-            {"chat_id": chat_id}, {"$set": {"language": language}}
+        await self.chats.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"language": language}},
         )
 
     async def get_language(self, context: Context) -> Optional[str]:
         chat_id = context.chat_id
-        chat_data = await self.chat_modes.find_one(
+        chat_data = await self.chats.find_one(
             {"chat_id": chat_id}, {"_id": 0, "language": 1}
         )
-        return chat_data.get("language")
 
     async def get_conversation_tracker_state(self, context: Context) -> bool:
         chat_id = context.chat_id
-        chat_data = await self.chat_modes.find_one(
+        chat_data = await self.chats.find_one(
             {"chat_id": chat_id}, {"_id": 0, "conversation_tracker": 1}
         )
         return chat_data.get("conversation_tracker", False)
@@ -103,21 +129,21 @@ class Chats(BaseModel):
             chat_data["_id"], chat_data["mode_name"], chat_data["mode_description"]
         )
 
-    async def get_mode_name_by_id(self, context: Context, mode_id: UUID) -> str:
+    async def get_mode_name_by_id(self, context: Context, mode_id: str) -> str:
         chat_id = context.chat_id
         chat_data = await self.chat_modes.find_one(
             {"chat_id": chat_id, "_id": ObjectId(mode_id)}
         )
         return chat_data["mode_name"]
 
-    async def set_chat_mode(self, context: Context, mode_id: UUID) -> None:
+    async def set_chat_mode(self, context: Context, mode_id: str) -> None:
         chat_id = context.chat_id
         await self.chat_modes.update_one(
             {"chat_id": chat_id, "_id": ObjectId(mode_id)},
             {"$set": {"active_mode": True}},
         )
 
-    async def delete_chat_mode(self, context: Context, mode_id: UUID) -> None:
+    async def delete_chat_mode(self, context: Context, mode_id: str) -> None:
         chat_id = context.chat_id
         await self.chat_modes.delete_one({"chat_id": chat_id, "_id": ObjectId(mode_id)})
 
@@ -138,7 +164,7 @@ class Chats(BaseModel):
     ) -> None:
         chat_id = context.chat_id
         user_handle = person.user_handle
-        await self.dialogs.update_one(
+        await self.user_introductions.update_one(
             {"chat_id": chat_id, "user_handle": user_handle},
             {"$set": {"introduction": introduction}},
             upsert=True,
@@ -147,13 +173,20 @@ class Chats(BaseModel):
     async def add_fact(self, context: Context, person: Person, fact: str) -> None:
         chat_id = context.chat_id
         user_handle = person.user_handle
-        await self.dialogs.update_one(
-            {"chat_id": chat_id, "user_handle": user_handle}, {"$push": {"facts": fact}}
+        await self.user_facts.update_one(
+            {"chat_id": chat_id, "user_handle": user_handle},
+            {"$push": {"facts": fact}},
+            upsert=True,
         )
 
-    async def clear_facts(self, context: Context, person: Person) -> None:
+    async def clear_facts(self, context: Context, facts_user_handle: str) -> None:
+        """Delete all facts associated with a user
+
+        Args:
+            context (Context): context of the chat
+            facts_user_handle (str): user handle to delete facts for
+        """
         chat_id = context.chat_id
-        user_handle = person.user_handle
-        await self.dialogs.update_one(
-            {"chat_id": chat_id, "user_handle": user_handle}, {"$set": {"facts": []}}
+        await self.user_facts.delete_one(
+            {"chat_id": chat_id, "user_handle": facts_user_handle}
         )
