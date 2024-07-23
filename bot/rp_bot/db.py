68df1549417ba4b651c3fbfdcde4ebdea7e1a0da
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-07-23 17:38:13 +0200

    Init new models in DB

--- bot/rp_bot/db.py
+++ bot/rp_bot/db.py
@@ -1,6 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from ..models.config import DefaultChatModes
 from .db_models.chats import Chats
+from .db_models.user_facts import UserFacts
+from .db_models.user_introductions import UserIntroductions
+from .db_models.chat_modes import ChatModes
 from .db_models.dialogs import Dialogs
 from .db_models.users import Users
 
@@ -19,5 +22,8 @@ class DB:
         client = AsyncIOMotorClient(uri)
         db = client[db_name]
         self.users = Users(db)
-        self.chats = Chats(db, default_chat_modes)
+        self.chats = Chats(db)
+        self.user_facts = UserFacts(db)
+        self.user_introductions = UserIntroductions(db)
+        self.chat_modes = ChatModes(db, default_chat_modes)
         self.dialogs = Dialogs(db)
