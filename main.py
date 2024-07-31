2b5e511ad2b965a1a2335be69b2369191b5b6473
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-07-31 18:32:16 +0200

    Move the db init to the rpbot

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import logging
 from decouple import config
 from pathlib import Path
 
-from bot.rp_bot.ai import AI
-from bot.rp_bot.db import DB
-from bot.rp_bot.auth import Auth
-from bot.models.localizer import Localizer
 from bot.rp_bot.bot import RPBot
 from bot.models.config import (
     TGConfig,
@@ -18,7 +14,6 @@ from bot.telegram.bot import TelegramBot
 
 
 def main():
-    # init logging
     logging.basicConfig(
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         level=logging.INFO,
@@ -33,31 +28,19 @@ def main():
     )
     ai_config = AIConfig.load(config_dir / "ai_config.yaml")
 
-    db = DB(
+    rp_bot = RPBot(
         db_user=config("DB_USER"),
         db_password=config("DB_PASSWORD"),
         db_host=config("DB_HOST"),
         db_port=config("DB_PORT"),
         db_name=config("DB_NAME"),
+        openai_api_key=config("OPENAI_API_KEY"),
+        translations=translations,
         default_chat_modes=default_chat_modes,
-    )
-
-    ai = AI(openai_api_key=config("OPENAI_API_KEY"), db=db, ai_config=ai_config)
-
-    localizer = Localizer(translations=translations)
-
-    auth = Auth(
+        ai_config=ai_config,
+        bot_config=bot_config,
         allowed_handles=config("ALLOWED_HANDLES", "").split(",") or None,
         admin_handles=config("ADMIN_HANDLES", "").split(",") or None,
-        db=db,
-    )
-
-    rp_bot = RPBot(
-        ai=ai,
-        db=db,
-        localizer=localizer,
-        auth=auth,
-        bot_config=bot_config,
         logger=logging.getLogger("RPBot"),
     )
 
