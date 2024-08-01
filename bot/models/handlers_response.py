8577a15cdeee9ac6b484c2d636101646d54632fd
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-08-01 17:17:59 +0200

    Make response models pydantic

--- bot/models/handlers_response.py
+++ bot/models/handlers_response.py
@@ -1,37 +1,21 @@
-from typing import Optional
+from typing import Optional, Dict
 from collections import OrderedDict
 from uuid import UUID
+from pydantic import BaseModel
 
 
-class KeyboardResponse:
-    def __init__(
-        self,
-        modes_dict: OrderedDict[UUID, str],
-        callback: str,
-        button_action: str,
-    ):
-        self.modes_dict = modes_dict
-        self.callback = callback
-        self.button_action = button_action
+class KeyboardResponse(BaseModel):
+    modes_dict: OrderedDict[UUID, str]
+    callback: str
+    button_action: str
 
 
-class CommandResponse:
-    def __init__(
-        self,
-        text,
-        kwargs: Optional[dict] = None,
-        keyboard: Optional[KeyboardResponse] = None,
-    ):
-        self.text = text
-        self.kwargs = kwargs
-        self.keyboard = keyboard
+class CommandResponse(BaseModel):
+    text: str
+    kwargs: Optional[Dict] = None
+    keyboard: Optional[KeyboardResponse] = None
 
 
-class LocalizedCommandResponse:
-    def __init__(
-        self,
-        localized_text: str,
-        keyboard: Optional[KeyboardResponse] = None,
-    ):
-        self.localized_text = localized_text
-        self.keyboard = keyboard
+class LocalizedCommandResponse(BaseModel):
+    localized_text: str
+    keyboard: Optional[KeyboardResponse] = None
