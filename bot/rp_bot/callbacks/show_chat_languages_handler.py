67c343d07b6c8946093e0117e059e7fc3d4064cb
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-08-11 16:03:42 +0200

    Implement the mixin logic for language selection

--- bot/rp_bot/callbacks/show_chat_languages_handler.py
+++ bot/rp_bot/callbacks/show_chat_languages_handler.py
@@ -4,26 +4,41 @@ from collections import OrderedDict
 from ...models.base_handlers import BaseCallbackHandler
 from ...models.handlers_response import KeyboardResponse, CommandResponse
 from ...models.handlers_input import Person, Context, Message
+from ..db import DB
 from ..auth import AllowedUser, NotBanned
 
 
-class CallbackHandler(BaseCallbackHandler):
+class ShowChatLanguagesMixin:
+    _show_chat_languages_callback = "show_chat_languages"
+
+    async def _get_chat_languages_keyboard(
+        self, db: DB, context: Context, callback_action: str
+    ) -> KeyboardResponse:
+        available_modes = await db.chat_modes.get_chat_modes(context)
+        modes_dict = OrderedDict(
+            {str(mode.id): str(mode.mode_name) for mode in available_modes}
+        )
+        return CommandResponse(
+            text="choose_mode",
+            keyboard=KeyboardResponse(
+                modes_dict=modes_dict,
+                callback=self._show_chat_languages_callback,
+                button_action=callback_action,
+            ),
+        )
+
+
+class CallbackHandler(BaseCallbackHandler, ShowChatLanguagesMixin):
     permissions = [AllowedUser, NotBanned]
-    callback_action = "show_chat_languages"
+    callback_action = ShowChatLanguagesMixin._show_chat_languages_callback
 
     async def get_response(
         self, person: Person, context: Context, message: Message, args: List[str]
     ) -> CommandResponse:
-        old_action = args[0]
-        available_languages = await self.localizer.get_supported_languages()
-        languages_dict = OrderedDict(
-            {language: language for language in available_languages}
-        )
         return CommandResponse(
-            text="choose_language",
-            keyboard=KeyboardResponse(
-                modes_dict=languages_dict,
-                callback=self.callback_action,
-                button_action=old_action,
+            keyboard=self._get_chat_languages_keyboard(
+                db=self.db,
+                context=context,
+                callback_action=args[0],
             ),
         )
--- bot/rp_bot/commands/language_handler.py
+++ bot/rp_bot/commands/language_handler.py
@@ -1,13 +1,13 @@
 from typing import List
-from collections import OrderedDict
 
 from ...models.base_handlers import BaseCommandHandler, CommandPriority
-from ...models.handlers_response import CommandResponse, KeyboardResponse
+from ...models.handlers_response import CommandResponse
 from ...models.handlers_input import Person, Context, Message
-from ..callbacks.show_chat_languages_handler import CallbackHandler
+from ..callbacks.set_chat_language_handler import CallbackHandler
+from ..callbacks.show_chat_languages_handler import ShowChatLanguagesMixin
 
 
-class CommandHandler(BaseCommandHandler):
+class CommandHandler(BaseCommandHandler, ShowChatLanguagesMixin):
     permissions = CallbackHandler.permissions
     command = "language"
     list_priority_order = CommandPriority.DEFAULT
@@ -15,15 +15,11 @@ class CommandHandler(BaseCommandHandler):
     async def get_response(
         self, person: Person, context: Context, message: Message, args: List[str]
     ) -> CommandResponse:
-        available_languages = await self.localizer.get_supported_languages()
-        languages_dict = OrderedDict(
-            {str(language): str(language) for language in available_languages}
-        )
         return CommandResponse(
             text="choose_language",
-            keyboard=KeyboardResponse(
-                modes_dict=languages_dict,
-                callback="show_chat_languages",
-                button_action="set_chat_language",
+            keyboard=self._get_chat_languages_keyboard(
+                db=self.db,
+                context=context,
+                callback_action=CallbackHandler.callback_action,
             ),
         )
