74ec6a9b7130cc308784bcedfcfa0445690c48e5
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-08-18 18:09:04 +0200

    Implement facts getter methods

--- bot/rp_bot/db_models/user_facts.py
+++ bot/rp_bot/db_models/user_facts.py
@@ -1,4 +1,6 @@
+from typing import List
 from motor.motor_asyncio import AsyncIOMotorDatabase
+
 from .base_models import BaseModel
 from ...models.handlers_input import Person, Context
 
@@ -7,6 +9,27 @@ class UserFacts(BaseModel):
     def __init__(self, db: AsyncIOMotorDatabase) -> None:
         super().__init__(db)
         self.user_facts = db.user_facts
+
+    async def get_chat_facts(self, context: Context) -> List[str, str]:
+        """
+        Return a list of facts for the current chat as a list
+        user_handle, fact
+        """
+        chat_id = context.chat_id
+        cursor = self.user_facts.find({"chat_id": chat_id})
+        facts = []
+        async for doc in cursor:
+            for fact in doc["facts"]:
+                facts.append((doc["user_handle"], fact))
+        return facts
+
+    async def get_user_facts(self, context: Context, person: Person) -> List[str]:
+        chat_id = context.chat_id
+        user_handle = person.user_handle
+        facts = await self.user_facts.find_one(
+            {"chat_id": chat_id, "user_handle": user_handle}
+        )
+        return facts.get("facts", [])
 
     async def add_fact(self, context: Context, person: Person, fact: str) -> None:
         chat_id = context.chat_id
