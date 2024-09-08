7b4c30843df67f8118e93c4c6e366208097b77e7
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-09-08 08:40:18 +0200

    Remove unused imports

--- bot/rp_bot/ai_agent/agent_tools/get_facts.py
+++ bot/rp_bot/ai_agent/agent_tools/get_facts.py
@@ -2,12 +2,11 @@ from langchain_core.runnables import chain
 
 from ...db import DB
 from ...prompt_manager import PromptManager
-from ....models.handlers_input import Person, Context
+from ....models.handlers_input import Context
 
 
 @chain
 async def get_chat_facts(prompt_manager: PromptManager, context: Context) -> str:
-    # TODO: pass the user handles
     return prompt_manager.compose_chat_facts_prompt(context)
 
 
@@ -15,6 +14,5 @@ async def get_chat_facts(prompt_manager: PromptManager, context: Context) -> str
 async def get_user_facts(
     db: DB, prompt_manager: PromptManager, context: Context, user_handle: str
 ) -> str:
-    # TODO: pass the user handles
     person = db.users.get_person_by_handle(user_handle)
     return prompt_manager.compose_user_facts_prompt(person, context)
