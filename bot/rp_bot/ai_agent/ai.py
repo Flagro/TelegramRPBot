83fa241d3b408249f9e1168432399ed95d6d598a
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-09-10 20:00:35 +0200

    Unify the get default model logic under _get_default_model_name method

--- bot/rp_bot/ai_agent/ai.py
+++ bot/rp_bot/ai_agent/ai.py
@@ -1,11 +1,11 @@
 import tiktoken
 import io
-from typing import AsyncIterator
+from typing import AsyncIterator, Literal
 from openai import OpenAI
 from langchain_openai import OpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 
-from ...models.config.ai_config import AIConfig
+from ...models.config.ai_config import AIConfig, Model
 from .agent_tools.describe_image import describe_image, ImageInformation
 
 
@@ -25,32 +25,39 @@ class AI:
             model=self._get_default_image_generation_model_name(),
         )
 
-    def _get_default_text_model_name(self) -> str:
+    def _get_default_model_name(
+        self, model_type: Literal["text", "vision", "image_generation"]
+    ) -> str:
+        params_dict = {
+            "text": {
+                "models_dict": self.ai_config.TextGeneration.Models,
+                "default_attr": "text_default",
+            },
+            "vision": {
+                "models_dict": self.ai_config.TextGeneration.Models,
+                "default_attr": "vision_default",
+            },
+            "image_generation": {
+                "models_dict": self.ai_config.ImageGeneration.Models,
+                "default_attr": "image_generation_default",
+            },
+        }
         first_model = None
-        for model in self.ai_config.TextGeneration.Models.values():
-            if model.text_default:
+        for model in params_dict[model_type]["models_dict"].values():
+            if getattr(model, params_dict[model_type]["default_attr"]):
                 return model.name
             if first_model is None:
                 first_model = model.name
         return first_model
+
+    def _get_default_text_model_name(self) -> str:
+        return self._get_default_model_name("text")
 
     def _get_default_vision_model_name(self) -> str:
-        first_model = None
-        for model in self.ai_config.TextGeneration.Models.values():
-            if model.vision_default:
-                return model.name
-            if first_model is None:
-                first_model = model.name
-        return first_model
+        return self._get_default_model_name("vision")
 
     def _get_default_image_generation_model_name(self) -> str:
-        first_model = None
-        for model in self.ai_config.ImageGeneration.Models.values():
-            if model.image_generation_default:
-                return model.name
-            if first_model is None:
-                first_model = model.name
-        return first_model
+        return self._get_default_model_name("image_generation")
 
     async def describe_image(
         self, in_memory_image_stream: io.BytesIO
