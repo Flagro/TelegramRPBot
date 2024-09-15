06abaaa3d39580900d4be6e3c04ef76641255dcf
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-09-15 16:23:17 +0200

    Add autofact CommandHandler

---
+++ bot/rp_bot/commands/autofact_handler.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from ...models.base_handlers import CommandPriority
+from ...models.handlers_response import CommandResponse
+from ...models.handlers_input import Person, Context, Message
+from ..rp_bot_handlers import RPBotCommandHandler
+from ..auth import GroupAdmin, AllowedUser, NotBanned
+
+
+class CommandHandler(RPBotCommandHandler):
+    permission_classes = (GroupAdmin, AllowedUser, NotBanned)
+    command = "autofact"
+    list_priority_order = CommandPriority.DEFAULT
+
+    async def get_response(
+        self, person: Person, context: Context, message: Message, args: List[str]
+    ) -> CommandResponse:
+        new_value = await self.db.chats.switch_auto_fact(context)
+        return CommandResponse(
+            text=(
+                "autofact_turned_on"
+                if new_value
+                else "autofact_turned_off"
+            ),
+        )
