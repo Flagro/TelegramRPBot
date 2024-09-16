1f4fa21d79a48005157a7e1f8138703e3a2b01f0
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-09-16 16:20:18 +0200

    Add basic implementation of compsoe_facts_based_on_messages

--- bot/rp_bot/ai_agent/agent_tools/autofact_generation.py
+++ bot/rp_bot/ai_agent/agent_tools/autofact_generation.py
@@ -1,9 +1,20 @@
+from typing import List
 from langchain_core.runnables import chain
 from langchain_openai import OpenAI
+from langchain_core.pydantic_v1 import BaseModel, Field
 
 from ...db import DB
 from ...prompt_manager import PromptManager
 from ....models.handlers_input import Context
+
+
+class UserFact(BaseModel):
+    user_fact: str = Field(description="a fact about the user")
+    user_handle: str = Field(description="the user handle")
+
+
+class ChatFacts(BaseModel):
+    user_facts: List[UserFact] = Field(description="list of facts about the user")
 
 
 @chain
@@ -12,3 +23,9 @@ async def check_if_facts_needed(prompt_manager: PromptManager, context: Context,
     facts_needed = await llm.invoke(prompt)
     # TODO: finish this implementation
     return False
+
+
+@chain
+async def compsoe_facts_based_on_messages(db: DB, prompt_manager: PromptManager, context: Context) -> ChatFacts:
+    # TODO: generate a chat facts list here
+    return []
