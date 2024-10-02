bedcaf6dc381dd5cca759db974110f7f8739c389
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-10-02 16:18:06 +0200

    Simplify the UserIntroductions methods

--- bot/rp_bot/db_models/user_introductions.py
+++ bot/rp_bot/db_models/user_introductions.py
@@ -12,18 +12,14 @@ class UserIntroductions(BaseDBModel):
     async def add_introduction(
         self, context: Context, person: Person, introduction: str
     ) -> None:
-        chat_id = context.chat_id
-        user_handle = person.user_handle
         await self.user_introductions.update_one(
-            {"chat_id": chat_id, "user_handle": user_handle},
+            {"chat_id": context.chat_id, "user_handle": person.user_handle},
             {"$set": {"introduction": introduction}},
             upsert=True,
         )
 
     async def get_user_introduction(self, context: Context, person: Person) -> str:
-        chat_id = context.chat_id
-        user_handle = person.user_handle
         introduction = await self.user_introductions.find_one(
-            {"chat_id": chat_id, "user_handle": user_handle}
+            {"chat_id": context.chat_id, "user_handle": person.user_handle}
         )
         return introduction.get("introduction", "")
