51aea20c675bce670dba862260257f0a0f5c3a40
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-11-04 11:31:55 +0100

    Add base implementation for moderation module

---
+++ bot/rp_bot/ai_agent/moderation.py
@@ -0,0 +1,14 @@
+def moderate_image(image_base64: str) -> bool:
+    """
+    Moderates the image and returns True if the image is safe
+    """
+    # TODO: implement this
+    return True
+
+
+def moderate_text(text: str) -> bool:
+    """
+    Moderates the text and returns True if the text is safe
+    """
+    # TODO: implement this
+    return True
