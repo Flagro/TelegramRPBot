fafdb5542433340eef07536c648076478a40c740
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-11-04 11:36:50 +0100

    Add moderate audio function and fix the parameters logic

--- bot/rp_bot/ai_agent/moderation.py
+++ bot/rp_bot/ai_agent/moderation.py
@@ -1,10 +1,21 @@
+import io
+
+
 class ModerationError(Exception):
     pass
 
 
-def moderate_image(image_base64: str) -> bool:
+def moderate_image(in_memory_image_stream: io.BytesIO) -> bool:
     """
     Moderates the image and returns True if the image is safe
+    """
+    # TODO: implement this
+    return True
+
+
+def moderate_audio(in_memory_audio_stream: io.BytesIO) -> bool:
+    """
+    Moderates the audio and returns True if the audio is safe
     """
     # TODO: implement this
     return True
