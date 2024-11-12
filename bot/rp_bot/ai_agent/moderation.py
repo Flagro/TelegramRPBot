6fba21c274aa58d14fb8db2e27b11aa048a3793b
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-11-12 18:04:54 +0100

    Make moderation a class

--- bot/rp_bot/ai_agent/moderation.py
+++ bot/rp_bot/ai_agent/moderation.py
@@ -3,33 +3,34 @@ import io
 from openai import OpenAI
 
 
-def moderate_image(in_memory_image_stream: io.BytesIO) -> bool:
-    """
-    Moderates the image and returns True if the image is safe
-    """
-    # TODO: implement this
-    return True
+class Moderation:
+    def moderate_image(self, in_memory_image_stream: io.BytesIO) -> bool:
+        """
+        Moderates the image and returns True if the image is safe
+        """
+        # TODO: implement this
+        return True
 
 
-def moderate_audio(in_memory_audio_stream: io.BytesIO) -> bool:
-    """
-    Moderates the audio and returns True if the audio is safe
-    """
-    # TODO: implement this
-    return True
+    def moderate_audio(self, in_memory_audio_stream: io.BytesIO) -> bool:
+        """
+        Moderates the audio and returns True if the audio is safe
+        """
+        # TODO: implement this
+        return True
 
 
-def moderate_text(text: str) -> bool:
-    """
-    Moderates the text and returns True if the text is safe
-    """
-    # TODO: moderation needs a class in order to use the right credentials
-    client = OpenAI()
-    response = client.moderations.create(
-        model="omni-moderation-latest",
-        input=text,
-    )
-    flagged = response['results'][0]['flagged']
-    if flagged:
-        return False
-    return True
+    def moderate_text(self, text: str) -> bool:
+        """
+        Moderates the text and returns True if the text is safe
+        """
+        # TODO: moderation needs a class in order to use the right credentials
+        client = OpenAI()
+        response = client.moderations.create(
+            model="omni-moderation-latest",
+            input=text,
+        )
+        flagged = response['results'][0]['flagged']
+        if flagged:
+            return False
+        return True
