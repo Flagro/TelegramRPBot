d936146d740f6ec230d5f1ad1c89e3550f62cbd1
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-11-22 18:36:46 +0100

    Add init method for base tool

--- bot/rp_bot/ai_agent/agent_tools/base_tool.py
+++ bot/rp_bot/ai_agent/agent_tools/base_tool.py
@@ -1,2 +1,8 @@
+from ....models.handlers_input import Person, Context, Message
+
+
 class BaseTool:
-    pass
+    def __init__(self, person: Person, context: Context, message: Message):
+        self.person = person
+        self.context = context
+        self.message = message
