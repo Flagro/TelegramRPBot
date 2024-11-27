5a48eb609e0ccf9cf21773c780bdd15f95003adf
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-11-27 18:13:09 +0100

    Add basic fields and abstract class to the BaseTool

--- bot/rp_bot/ai_agent/agent_tools/base_tool.py
+++ bot/rp_bot/ai_agent/agent_tools/base_tool.py
@@ -1,8 +1,16 @@
+from abc import ABC, abstractmethod
 from ....models.handlers_input import Person, Context, Message
 
 
-class BaseTool:
+class BaseTool(ABC):
+    name: str
+    description: str
+
     def __init__(self, person: Person, context: Context, message: Message):
         self.person = person
         self.context = context
         self.message = message
+
+    @abstractmethod
+    async def run(self, *args, **kwargs):
+        pass
