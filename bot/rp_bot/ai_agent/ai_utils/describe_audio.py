49b9a71ea764a50ae9c01abb61377726a1f7b6d9
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-11-30 14:46:09 +0100

    Implement DescribeImageUtililty utility

---
+++ bot/rp_bot/ai_agent/ai_utils/describe_audio.py
@@ -0,0 +1,39 @@
+import io
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.output_parsers import JsonOutputParser
+
+from ..models_toolkit import ModelsToolkit
+from ....models.handlers_input import Person, Context, Message
+
+
+class AudioInformation(BaseModel):
+    # TODO: move this to prompt manager
+    audio_description: str = Field(description="a short description of the audio")
+
+
+async def describe_audio(in_memory_image_stream: io.BytesIO) -> AudioInformation:
+    # Encode in base64:
+    parser = JsonOutputParser(pydantic_object=AudioInformation)
+
+    # TODO: implement moderation
+
+    return AudioInformation(
+        audio_description="an audio"
+    )
+
+
+class DescribeImageUtililty:
+    def __init__(
+        self,
+        person: Person,
+        context: Context,
+        message: Message,
+        models_toolkit: ModelsToolkit,
+    ):
+        self.person = person
+        self.context = context
+        self.message = message
+        self.models_toolkit = models_toolkit
+
+    def run(self, in_memory_image_stream: io.BytesIO) -> AudioInformation:
+        return describe_audio(in_memory_image_stream)
