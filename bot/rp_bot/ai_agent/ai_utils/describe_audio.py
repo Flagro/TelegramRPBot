2a1560a00d77b2d1c4b4eb11ed49c8286b24f442
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-12-01 08:12:16 +0100

    Remove unused imports from utility modules

--- bot/rp_bot/ai_agent/ai_utils/describe_audio.py
+++ bot/rp_bot/ai_agent/ai_utils/describe_audio.py
@@ -3,8 +3,6 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.output_parsers import JsonOutputParser
 
 from .base_utility import BaseUtility
-from ..models_toolkit import ModelsToolkit
-from ....models.handlers_input import Person, Context, Message
 
 
 class AudioInformation(BaseModel):
