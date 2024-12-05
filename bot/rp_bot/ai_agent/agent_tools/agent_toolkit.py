5646eee7fd545169c3d5cd3ba079febcc0a94330
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-12-05 10:27:33 +0400

    Add prompt manager in the toolkit init

--- bot/rp_bot/ai_agent/agent_tools/agent_toolkit.py
+++ bot/rp_bot/ai_agent/agent_tools/agent_toolkit.py
@@ -4,7 +4,7 @@ from .generate_image import ImageGeneratorTool
 from .get_facts import GetChatFactsTool, GetUserFactsTool
 from .get_response import GetResponseTool
 from ..models_toolkit import ModelsToolkit
-
+from ...prompt_manager import PromptManager
 from ....models.handlers_input import Person, Context, Message
 
 
@@ -15,19 +15,26 @@ class AIAgentToolkit:
         context: Context,
         message: Message,
         models_toolkit: ModelsToolkit,
+        prompt_manager: PromptManager,
     ):
         self.check_if_facts_needed = CheckIfFactsNeededTool(
-            person, context, message, models_toolkit
+            person, context, message, models_toolkit, prompt_manager
         )
         self.compose_facts_based_on_messages = ComposeFactsBasedOnMessagesTool(
-            person, context, message, models_toolkit
+            person, context, message, models_toolkit, prompt_manager
         )
         self.check_engage_needed = CheckEngageNeededTool(
-            person, context, message, models_toolkit
+            person, context, message, models_toolkit, prompt_manager
         )
         self.image_generator = ImageGeneratorTool(
-            person, context, message, models_toolkit
+            person, context, message, models_toolkit, prompt_manager
         )
-        self.get_chat_facts = GetChatFactsTool(person, context, message, models_toolkit)
-        self.get_user_facts = GetUserFactsTool(person, context, message, models_toolkit)
-        self.get_response = GetResponseTool(person, context, message, models_toolkit)
+        self.get_chat_facts = GetChatFactsTool(
+            person, context, message, models_toolkit, prompt_manager
+        )
+        self.get_user_facts = GetUserFactsTool(
+            person, context, message, models_toolkit, prompt_manager
+        )
+        self.get_response = GetResponseTool(
+            person, context, message, models_toolkit, prompt_manager
+        )
