41189fc9e6f59f011f27546efb371abf196cd7d6
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2025-05-13 09:14:28 +0200

    Refactor: Add db parameter to BaseTool constructor for improved database handling

--- bot/rp_bot/ai_agent/agent_tools/base_tool.py
+++ bot/rp_bot/ai_agent/agent_tools/base_tool.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from omnimodkit import ModelsToolkit
+from motor.motor_asyncio import AsyncIOMotorDatabase
 from ....models.handlers_input import Person, Context, Message
 from ...prompt_manager import PromptManager
 
@@ -13,12 +14,14 @@ class BaseTool(ABC):
         person: Person,
         context: Context,
         message: Message,
+        db: AsyncIOMotorDatabase,
         models_toolkit: ModelsToolkit,
         prompt_manager: PromptManager,
     ):
         self.person = person
         self.context = context
         self.message = message
+        self.db = db
         self.models_toolkit = models_toolkit
         self.prompt_manager = prompt_manager
 
